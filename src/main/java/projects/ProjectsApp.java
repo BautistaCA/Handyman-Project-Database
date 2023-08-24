0537056ca754a760c1b24c74370df2b020723533
BautistaCA <bautistaca23@gmail.com>
2023-08-23 20:03:04 -0700

    added projects-schema for Week08's assignment

--- src/main/java/projects/ProjectsApp.java
+++ src/main/java/projects/ProjectsApp.java
@@ -1,14 +1,66 @@
 package projects;
 
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
 
-
-import projects.dao.DbConnection;
-
+import projects.exception.DbException;
+//stopped at 9
 public class ProjectsApp {
+private Scanner scanner = new Scanner(System.in);
 
+	//@formatter:off
+		private	List<String> operations = List.of(
+					"1) Add a project"
+					);
+		
+	//@formatter:on
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-DbConnection.getConnection();
+new ProjectsApp().processUserSelections();
+
 	}
 
+	private void processUserSelections() {
+		boolean done = false;
+		while(!done) {
+			try {
+				int selection = getUserSelection();
+			} catch (Exception e) {
+		System.out.println("\nError: " + e + "Try again.");
+			}
+		}
+		
+	}
+
+	private int getUserSelection() {
+		printOperation();
+		
+		Integer input = getIntInput("Enter a menu selection");
+		
+		return Objects.isNull(input) ? -1 : input;
+	}
+
+	private Integer getIntInput(String prompt) {
+		String input = getStringInput(prompt);
+		
+		if(Objects.isNull(input)) {
+			return null;
+		}
+		try {
+			return Integer.valueOf(input);
+		} catch (NumberFormatException e) {
+			throw new DbException(input + " is not a valid number.");
+		}
+	}
+
+	private String getStringInput(String prompt) {
+		System.out.print(prompt + ":");
+		String input = scanner.nextLine();
+		return input.isBlank() ? null : input.trim();
+	}
+
+	private void printOperation() {
+		operations.forEach(line -> System.out.println("   " + line));
+		
+	}
 }
